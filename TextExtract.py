6a48d90b85d436c8e9fcc86ea9ccc2f61df7b870
406X <406X>
2019-05-28 16:36:06 +0800

    Fixed

--- TextExtract.py
+++ TextExtract.py
@@ -3,13 +3,13 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import html5lib
 import string
+import re
 
 link1 = "https://www.textise.net/showText.aspx?strURL=http%3a%2f%2f"
 link2 = "google.com"
 link = link1 + link2
 req = urllib.request.Request(link,headers ={'User-Agent': "Mozilla/5.0"})
 text = urllib.request.urlopen(req)
-
 
 Text = BeautifulSoup(text,'html5lib')
 for script in Text(["script", "style"]):
@@ -29,23 +29,22 @@ def getHash(input):
         hash+=((count)**2)*(ord(x))
         count+=1
 
-    hash = hash%tableSize
+    hash = hash % tableSize
 
     return hash
 
 def retrieveIndex(word, hash):
 
-    if(hTable[hash]==None):
+    if(hTable[hash] == None):
         return -1
-    elif(type(hTable[hash][0]) == list):
+    elif( type( hTable[hash][0] ) == list):
         for x in hTable[hash]:
-            if(x[0]==word):
+            if(x[0] == word):
                 return x[1]
             elif(x[0] == word):
                 return x[1]
-    elif(hTable[0] == word ):
-                return hTable[hash][1]
-
+    elif(hTable[hash][0] == word ):
+            return hTable[hash][1]
     return -1
 
 def addIndex(word, index, hash):
@@ -54,30 +53,65 @@ def addIndex(word, index, hash):
     elif ( type(hTable[hash][0]) == list):
         hTable[hash].append([word, index])
     else:
-        hTable[hash] = [hTable[hash], [word, index]]
+        hTable[hash] = [ hTable[hash], [word, index] ]
 
 
-#Need to remove symbols, urls and stop words
-for x in Text.stripped_strings:
-    x.translate(str.maketrans("","",string.punctuation))
+def string_removeURL(input):
+    input = re.sub("www.[\w]", "", input)
+    input = re.sub("[\w]+.net", "", input)
+    input = re.sub("[\w]+.org", "", input)
+    input = re.sub("[\w]+.com", "", input)
+    return re.sub("http[^\s]+", "", input)
 
-    #Removes punctuations
+def string_removePunctuation(input):
     cleanStr = ""
-    for c in x:
+
+    for c in input:
         if c in string.ascii_letters or c in " ":
             cleanStr = cleanStr + c
 
-    for y in cleanStr.split():
-        hash = getHash(y)
-        index =retrieveIndex(y,hash)
-        if(index == -1):
-            wordList[wordCount] = [y,1]
-            addIndex(y,wordCount,hash)
-            wordCount+=1
-        else:
-            wordList[index][1]+=1
+    return cleanStr
 
-for x in wordList:
-    print(x)
+def string_removeInList(input, list):
+    cleanStr=''
+    for c in input.split():
+        if(c.lower() not in list):
+            cleanStr = cleanStr + " " + c
+
+    return cleanStr
+
+def string_normalize(input):
+    cleanStr = input.lower()
+    return cleanStr
+
+stops = open("stopword.txt", encoding='utf-8')
+stopList = []
+
+for c in stops:
+    stopList.append(c.strip())
+
+for x in Text.stripped_strings:
+
+    cleanStr = x
+    cleanStr = string_removeURL(cleanStr)
+    cleanStr = string_removeInList(cleanStr, stopList)
+    cleanStr = string_removePunctuation(cleanStr)
+    cleanStr = string_normalize(cleanStr)
+
+    if(cleanStr!=''):
+        for y in cleanStr.split():
+            hash = getHash(y)
+
+            index =retrieveIndex(y,hash)
+
+            if(index == -1):
+                wordList[wordCount] = [y,1]
+                addIndex(y,wordCount,hash)
+                wordCount+=1
+            else:
+                wordList[index][1]+=1
+
+for x in range(wordCount):
+    print(wordList[x])
 
 
