317b90f6a48c8abff6737ac21f3a903519f1e07a
406X <406X>
2019-05-29 11:36:14 +0800

    Added new features

--- Main.py
+++ Main.py
@@ -10,6 +10,7 @@ from threading import Thread
 
 #Variables
 numTransits = 3
+numRoutes = 3
 source = "Kuala Lumpur"
 dest= "Dubai"
 cities = { "Beijing","Dubai","Los Angeles","Tokyo","London","Hong Kong","Amsterdam","Incheon","Istanbul","Singapore","Argentina","Delhi","Madrid","Kuala Lumpur"}
@@ -50,6 +51,7 @@ def showRoute(route):
 
 def start():
     global dest
+    global numRoutes
     #global source
     global numTransits
     list = cities.copy()
@@ -69,6 +71,9 @@ def start():
     list.remove(dest)
     num = input("Number of transits to make (1-5) : ")
     numTransits = int(num)
+
+    num = input("How many routes do you want? : ")
+    numRoutes = int(num)
 
 def calculateSentimentScores(routes):
 
@@ -92,7 +97,12 @@ class thread_Thread(threading.Thread):
         for y in range(length):
             if (type(x[y]) == float):
                 continue
-            totalSentimentScore += TE.getSentiment(x[y])
+            score = TE.getSentiment(x[y])
+            if(score>10):
+                score = 10
+            if(score<-10):
+                score=-10
+            totalSentimentScore += score
         x.append(totalSentimentScore)
 
     def join(self):
@@ -124,17 +134,66 @@ for x in routes:
 for r in Thread2:
     r.join()
 
-routes = RT.quickSort( routes, 0 , len(routes)-1, val = len(routes[0])-2 )
+length = len(routes[0])-2
+routes = RT.quickSort( routes, 0 , len(routes)-1, val = length )
 
-bestRoute = []
-for x in routes[0]:
-    if( type(x) == float):
+lengthRoutes = len(routes)
+if(numRoutes>len(routes)):
+    print("## Requested " + str(numRoutes) + " routes but only " + str(lengthRoutes) +" Available." )
+
+#Probability calculation based on distance alone
+totalDist = 0.0
+for x in range(numRoutes):
+    if(x > lengthRoutes-1):
         break
-    bestRoute.append(x)
+    totalDist += routes[x][length]
+
+totalWeight=0.0
+for x in range(numRoutes):
+    if(x > lengthRoutes-1):
+        break
+    totalWeight += 1/(routes[x][length]/totalDist)
+
+for x in range(numRoutes):
+    if(x > lengthRoutes-1):
+        break
+    print(str(x+1) + ". Route: " + str(routes[x][0:numTransits]) + " | Distance: {:0.5f}KM".format(routes[x][length]) +" | Sentiment Score: " + str(routes[x][length+1]) +" | Probability: {:0.5f}".format((1/(routes[x][length]/totalDist)/(totalWeight/100))) +"%" )
+
 
 end = time.time()
-print("Time:")
-print(end - startTime)
+print("Time: "+ str(end - startTime))
 
-showRoute(bestRoute)
+numChoice = 0
+while(1==1):
+    num = input("Which route would you like to display? (1-"+ str(numRoutes)+"): ")
+    if(int(num) <= 0 or int(num)>numRoutes):
+        print("Invalid number")
+        continue
+    else:
+        numChoice = int(num)-1
+        chosenRoute = []
+
+        for x in routes[numChoice]:
+            if (type(x) == float):
+                break
+            chosenRoute.append(x)
+        showRoute(chosenRoute)
+
+    choice ='Y'
+    while(1==1):
+        choice = input("Do you want to diplay another route?(Y/N): ")
+        if (choice == "Y" or choice == "y"):
+            break
+        elif (choice == "N" or choice == "n"):
+            print("Ending Program...")
+            break
+        else:
+            print("Invalid Option")
+            continue
+
+    if (choice == "N" or choice == "n"):
+        break
+
+
+
 
